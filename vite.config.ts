70e1afea2cb04467197e82d1b3ca6691dce30f18
Antony Emongoluk <146765794+Mistaken-identity@users.noreply.github.com>
2025-08-11 14:30:17 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
+    const env = loadEnv(mode, '.', ''); base: '/bigi-project/', // <--This fixes the white screen
     return {
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
